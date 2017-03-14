0b81383f5066950e4139723e0eac58878b1b1463
DESKTOP-7VD6D5S\besto <bestofall77@gmail.com>
2017-03-14 12:40:58 +0900

    init

---
+++ app/src/main/java/com/hyugnmin/android/firebasechat/Room.java
@@ -0,0 +1,25 @@
+package com.hyugnmin.android.firebasechat;
+
+/**
+ * Created by besto on 2017-03-13.
+ */
+public class Room {
+    String key;
+    String title;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
